b575708a7017ca128c46695f78f49f91f8a2ca40
Milan Wiedemann <milan.wiedemann@gmail.com>
2020-04-13 18:31:02 +0100

    add some code, probably delete this repo soon

--- scripts/xx-join-datasets.R
+++ scripts/xx-join-datasets.R
@@ -1,5 +1,40 @@
-# Example how to join different data sets by an ID variable
+# Example how to join different datasets by an ID variable
+# For help see chapter on Relational Data in R4DS: https://r4ds.had.co.nz/relational-data.html
 
-# Load packages ----
+# Load Packages ----
+
+# tidyverse for data manipulation and visualisation
 # install.packages("tidyverse")
 library(tidyverse)
+
+# haven for loading data into R
+# install.packages("haven")
+library(haven)
+
+
+# Load data ----
+# Load file: mcs4_aspirations_classifications.sav
+data_01 <- read_sav("mch-data/UKDA-6411-spss/spss/spss19/mcs4_aspirations_classifications.sav")
+
+data_01 <- data_01 %>% 
+  select()
+
+mcs4_cm_assessment <- read_sav("mch-data/UKDA-6411-spss/spss/spss19/mcs4_cm_assessment.sav")
+mcs4_derived_variables <- read_sav("mch-data/UKDA-6411-spss/spss/spss19/mcs4_derived_variables.sav")
+
+data_01 <- mcs4_cm_assessment %>% 
+  select(MCSID, DCCSEX00)
+
+
+mcs4_assessment %>% 
+  select(MCSID, DCCSEX00, DCAGEY00)
+
+data_02 <- mcs4_derived_variables %>% 
+ #  select(MCSID, DPKESS00) # here we select two vars
+
+
+# combine data 01 with data 02
+data_join <- left_join(data_01, data_02, by = "MCSID")
+
+
+
